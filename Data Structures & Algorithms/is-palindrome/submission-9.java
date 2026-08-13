c052bc68d3ad57af934fe53e5456cca58a2cdf3f
Erika Salvador <erikasal832@gmail.com>
2026-08-13 15:14:22 -0500

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        //s to lowercase
+        //left = 0; right = s.length() - 1;
+        //while(left < right)
+        //if char at left is not alphanum, keep increasing until alphanum -> repeat process with right except decrease right
+        //if char at left != char at right -> return false
+        //return true; 
+
+        s = s.toLowerCase();
+        int left = 0; 
+        int right = s.length() - 1; 
+        String alphanum = "abcdefghijklmnopqrstuvwxyz0987654321";
+
+        while(left < right) {
+            while(left < s.length() && alphanum.indexOf(s.charAt(left)) == -1){
+                left++; 
+            }
+            while(right >= 0 && alphanum.indexOf(s.charAt(right)) == -1){
+                right--; 
+            }
+
+            if (right >= 0 && left < s.length() && s.charAt(right) != s.charAt(left)) {
+                return false; 
+            }
+            left++; 
+            right--; 
+        }
+
+        return true; 
+    }
+}
